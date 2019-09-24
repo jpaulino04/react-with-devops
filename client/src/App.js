17a8b098575efdb6fc38f5b8da98171dbeafc05b
Jose Paulino <jpaulino04@msn.com>
2019-09-24 12:49:39 -0400

    re-initialized repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-//redux
-import { Provider } from 'react-redux'
-import store from './store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App pt-3 bg-dark">
-        <h1 className="text-center text-white"> Welcome to DevOps </h1>
-      </div>
-    </Provider>    
+    <div className="App pt-3 bg-dark">
+      <h1 className="text-center text-white"> Welcome to DevOps </h1>
+    </div>  
   );
 }
 
